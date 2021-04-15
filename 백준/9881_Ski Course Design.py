b2da7be6c5e400b73456a0f29947350015b045d7
JunkiKim <wemanse@gmail.com>
2021-04-15 23:10:39 +0900

    0415

---
+++ 백준/9881_Ski Course Design.py
@@ -0,0 +1,17 @@
+N = int(input())
+hills = []
+for i in range(N):
+    hills.append(int(input()))
+hills.sort()
+costs = 1e9
+for i in range(0, 83):
+    tempcost = 0
+
+    for hill in hills:
+        if hill < i:
+            tempcost += ((i - hill) * (i - hill))
+        elif hill > i + 17:
+            tempcost += ((hill - (i + 17)) * (hill - (i + 17)))
+
+    costs = min(costs, tempcost)
+print(costs)
