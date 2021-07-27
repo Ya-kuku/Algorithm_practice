2d11f3bc020523ada3683ce473c8e85b5e5be7b7
JunkiKim <wemanse@gmail.com>
2021-07-27 23:50:18 +0900

    0727

---
+++ 프로그래머스/소수 만들기.py
@@ -0,0 +1,28 @@
+from itertools import combinations
+def getPrimeList(n):
+    seive = [True] * n
+
+    for i in range(2,int(n**0.5)+1):
+        if seive[i]:
+            # 처음 소수의 다음 배수 부터 범위 지정
+            for j in range(2*i,n,i):
+                seive[j] = False
+
+    return [i for i in range(2,n) if seive[i]]
+
+def solution(nums):
+    answer = 0
+
+    pos_nums = list(combinations(nums,3))
+
+    num = []
+    for pos in pos_nums:
+        num.append(sum(pos))
+
+    seive = getPrimeList(3000)
+
+
+    for i in num:
+        if i in seive:
+            answer += 1
+    return answer
