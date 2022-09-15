94f93e67e17985e349ef1c9a6516f0b60b4397fb
JunkiKim <wemanse@gmail.com>
2022-09-15 23:43:59 +0900

    long time no see

---
+++ 프로그래머스/연속된자리가 안되요.py
@@ -0,0 +1,13 @@
+def solution(arr):
+    answer = []
+    answer.append(arr[0])
+    for i in range(1,len(arr)):
+        if arr[i] == answer[-1]:
+            continue
+        else:
+            answer.append(arr[i])
+    print(answer)
+    return answer
+
+arr = [1,1,3,3,0,1,1]
+solution(arr)
